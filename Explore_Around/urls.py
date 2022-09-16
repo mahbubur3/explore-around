2574c81a5ee233789e186753fc6d575a22d65ad3
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-09-16 10:49:41 +0600

    show output and created urls file

--- Explore_Around/urls.py
+++ Explore_Around/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from .import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('Account_Management.urls')),
     path('blog/', include('Blog_Management.urls')),
+    path('', views.index, name='index'),
 ]
