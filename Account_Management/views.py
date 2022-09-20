0c3cd7407d9b49531a269dad97d4f4c200c3b7f5
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-09-20 22:05:05 +0600

    created signup view

--- Account_Management/views.py
+++ Account_Management/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def signup(request):
+    form = UserCreationForm()
+    registered = False
+
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            registered = True
+
+    return render(request, 'Account_Management/signup.html', {'form': form, 'registered': registered})
