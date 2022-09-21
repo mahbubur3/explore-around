48ca9d5b083917c287ce7dff3bee48b45e5c8b61
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-09-21 20:13:34 +0600

    created edit profile form

--- Account_Management/forms.py
+++ Account_Management/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 
 
@@ -11,3 +11,9 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+    
+class EditUserProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'password')
