465326525a855dbcb695e14cfb88e097c635cb0e
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-09-21 12:59:04 +0600

    setup logout url

--- Account_Management/urls.py
+++ Account_Management/urls.py
@@ -7,4 +7,5 @@ app_name = 'Account_Management_App'
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
+    path('signout/', views.signout, name='signout'),
 ]
