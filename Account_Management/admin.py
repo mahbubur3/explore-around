70126c61f95e2ddd29fb1be746b1268b2feb851b
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-09-21 10:50:52 +0600

    updated on admin file

--- Account_Management/admin.py
+++ Account_Management/admin.py
@@ -3,10 +3,3 @@ from .models import UserProfile
 
 
 admin.site.register(UserProfile)
-
-
-# admin username = user 
-# admin password = user123
-
-# user1 username = rahat1
-# user1 password = rahman123123
