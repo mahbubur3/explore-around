4f3e2b518429523b3f2bb6012a340d83641afea0
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-09-22 11:29:31 +0600

    setup change password url

--- Account_Management/urls.py
+++ Account_Management/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('signout/', views.signout, name='signout'),
     path('profile/', views.user_profile, name='user_profile'),
     path('edit-profile/', views.edit_user_profile, name='edit_profile'),
+    path('change-password/', views.change_password, name='change_password'),
 ]
