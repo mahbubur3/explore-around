65a9003ece1d53694c10dc1475b7f97786d76663
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-09-22 15:22:38 +0600

    created add profile picture form

--- Account_Management/forms.py
+++ Account_Management/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
+from .models import UserProfile
 
 
 class SignupForm(UserCreationForm):
@@ -17,3 +18,9 @@ class EditUserProfileForm(UserChangeForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'email', 'password')
+
+    
+class ProfilePicture(forms.ModelForm):
+     class Meta:
+        model = UserProfile
+        fields = ['profile_picture', ]
