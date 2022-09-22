ca1ba2eabca3a48d3d0d02fc26e2c6e39f17f142
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-09-22 19:27:10 +0600

    updated write blog view

--- Blog_Management/views.py
+++ Blog_Management/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from django.views.generic import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Blog
+import uuid
+from django.urls import reverse
 
 
 def blogs(request):
@@ -13,4 +15,14 @@ class WriteBlog(LoginRequiredMixin, CreateView):
     model = Blog
     template_name = 'Blog_Management/write_blog.html'
     fields = ('blog_title', 'blog_content', 'blog_image', )
+
+    def form_valid(self, form):
+        blog_obj = form.save(commit=False)
+        blog_obj.author = self.request.user
+        title = blog_obj.blog_title
+
+        # add 4 unique id in url
+        blog_obj.slug = title.replace(" ", "-") + "-" + str(uuid.uuid4())
+        blog_obj.save()
+        return HttpResponseRedirect(reverse('index'))
 
