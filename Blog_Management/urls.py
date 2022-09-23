28cb27ccb7ee9df8d0f73a9b9f17927800104464
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-09-23 12:28:58 +0600

    added read more button and funcionality

--- Blog_Management/urls.py
+++ Blog_Management/urls.py
@@ -7,4 +7,5 @@ app_name = 'Blog_Management_App'
 urlpatterns = [
     path('', views.AllBlog.as_view(), name='all_blog'),
     path('write/', views.WriteBlog.as_view(), name='write_blog'),
+    path('<str:slug>', views.full_blog, name='full_blog'),
 ]
