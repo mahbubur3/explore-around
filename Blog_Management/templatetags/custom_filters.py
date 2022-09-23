7de19ceec37f486774cffb93da9fcd0f8c777fe9
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-09-23 17:49:58 +0600

    updated

--- Blog_Management/templatetags/custom_filters.py
+++ Blog_Management/templatetags/custom_filters.py
@@ -4,6 +4,6 @@ from django import template
 register = template.Library()
 
 def range_filter(value):
-    return value[0:300] + "......"
+    return value[0:400] + "......"
 
 register.filter('range_filter', range_filter)
