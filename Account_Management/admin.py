b4ceaf955deefa835f2bce2798adbe8f87fa68b1
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-09-27 07:38:37 +0600

    added username and password

--- Account_Management/admin.py
+++ Account_Management/admin.py
@@ -3,3 +3,18 @@ from .models import UserProfile
 
 
 admin.site.register(UserProfile)
+
+
+# temp
+"""
+admin username = user 
+admin password = user123
+user1 username = rahat1
+user1 password = rahman123567
+user2 username = tithi36
+user2 password = thayat4567
+user3 username = sakib6
+user3 password = sr1234567
+user4 username = mahin
+user4 password = ma1234567
+"""
