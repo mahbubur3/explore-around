790112fd45c07196db5e0acfbbac334336e6c338
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-09-29 18:48:12 +0600

    updated via some files

--- Account_Management/forms.py
+++ Account_Management/forms.py
@@ -5,19 +5,17 @@ from .models import UserProfile
 
 
 class SignupForm(UserCreationForm):
-    first_name = forms.TextInput()
-    last_name = forms.TextInput()
     email = forms.EmailField(label="Email Address", required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2')
 
     
 class EditUserProfileForm(UserChangeForm):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'username', 'email', 'password')
+        fields = ('username', 'email', 'password')
 
     
 class ProfilePictureForm(forms.ModelForm):
